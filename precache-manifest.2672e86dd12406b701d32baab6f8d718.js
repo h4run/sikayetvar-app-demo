33e12c7d115f115d463deaea136820eab1754bd7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-24 10:20:22 +0000

    Deploy h4run/sikayetvar-app-demo to github.com/h4run/sikayetvar-app-demo.git:gh-pages

---
+++ precache-manifest.2672e86dd12406b701d32baab6f8d718.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0815319af88aadcc97c8e984f2c67b8",
+    "url": "/sikayetvar-app-demo/index.html"
+  },
+  {
+    "revision": "9eb28ce866c48efdf8da",
+    "url": "/sikayetvar-app-demo/static/js/2.8bc9face.chunk.js"
+  },
+  {
+    "revision": "bb0d9eec61ee5484410f03345faddf5d",
+    "url": "/sikayetvar-app-demo/static/js/2.8bc9face.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a312c944b9b668fb271e",
+    "url": "/sikayetvar-app-demo/static/js/main.2c6382b4.chunk.js"
+  },
+  {
+    "revision": "7c279dc72e1d5c196daf",
+    "url": "/sikayetvar-app-demo/static/js/runtime-main.8f1be6cf.js"
+  },
+  {
+    "revision": "84d95e0e3cfc6ac675e91c3c44519f74",
+    "url": "/sikayetvar-app-demo/static/media/logo.84d95e0e.png"
+  },
+  {
+    "revision": "940ad82be090119da35369e9bd5a53fe",
+    "url": "/sikayetvar-app-demo/static/media/logo_bg.940ad82b.png"
+  }
+]);
